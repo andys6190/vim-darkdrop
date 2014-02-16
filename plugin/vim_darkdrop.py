1b3a6e795eb0b772dbd847258b06aca1117c61cb
Andy Siradas <andys6190@gmail.com>
2014-02-16 14:29:04 -0500

    buffer encryption working

--- plugin/vim_darkdrop.py
+++ plugin/vim_darkdrop.py
@@ -1,16 +1,18 @@
 from crypto_functions import *
+from getpass import getpass
 import hashlib
 import vim
 
 def DarkDropFile():
-    password = 'testpassword'
+    password = getpass()
     key = hashlib.sha256(password).digest()
 
-    filename = str(random.randint(0, 0xFF)) + '.tmp'
+    homedir = os.path.expanduser('~')
+    filename = homedir + '/.securenotes/' + str(random.randint(0, 0xFF)) + '.tmp'
     noteDump = open(filename, 'w')
     noteDump.write('\n'.join(vim.current.buffer))
 
     encrypt_file(key, filename);
-    os.remove(os.path.abspath(filename))
+    os.remove(filename)
     noteDump.close()
 
